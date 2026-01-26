214a2771d926568572491079a81aff6f0936abbc
Frank xavio <frank2006x@gmail.com>
2026-01-26 08:37:05 +0530

    Create 2796-AllowOneFunctionCall

---
+++ 2796-AllowOneFunctionCall/2796-AllowOneFunctionCall.js
@@ -0,0 +1,21 @@
+// Last updated: 1/26/2026, 8:37:04 AM
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+var once = function(fn) {
+    var i=false;
+    return function(...args){
+        if(i) return undefined;
+        i=true
+        return fn(...args);
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
