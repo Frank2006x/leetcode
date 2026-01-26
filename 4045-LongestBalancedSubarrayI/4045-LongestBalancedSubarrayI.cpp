86d1ad795272db3bb744fd1ec4bf6fe3e2efc701
Frank xavio <frank2006x@gmail.com>
2026-01-26 08:35:44 +0530

    Create 4045-LongestBalancedSubarrayI

---
+++ 4045-LongestBalancedSubarrayI/4045-LongestBalancedSubarrayI.cpp
@@ -0,0 +1,25 @@
+// Last updated: 1/26/2026, 8:35:43 AM
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int e = 0;
+            int o = 0;
+            unordered_map<int, int> map;
+            for (int j = i; j < nums.size(); j++) {
+                if (map.find(nums[j]) == map.end()) {
+                    if (nums[j] % 2 == 0) {
+                        e++;
+                    } else{
+                        o++;
+                    }
+                }
+                map[nums[j]]++;
+                if (e == o)
+                    res = max(res, j - i + 1);
+            }
+        }
+        return res;
+    }
+};
