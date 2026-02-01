7f06599d7af02d552b3a28236bd01c9cbd59c29c
Frank xavio <frank2006x@gmail.com>
2026-02-01 13:54:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.11%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,13 +4,13 @@ public:
         int n=nums.size();
         if(n==1) return nums[0];
         if(n==2) return max(nums[0],nums[1]);
-        vector<int> robo(n,-1);
-        robo[0]=nums[0];
-        robo[1]=max(nums[1],nums[0]);
-
-        for(int i=2;i<n;i++){
-            robo[i]=max(robo[i-1],robo[i-2]+nums[i]);
+        int dp[n];
+        dp[0]=nums[0];
+        dp[1]=nums[1];
+        dp[2]=nums[0]+nums[2];
+        for(int i=3;i<n;i++){
+            dp[i]=max(dp[i-3],dp[i-2])+nums[i];
         }
-        return robo[n-1];
+        return max(dp[n-1],dp[n-2]);
     }
 };
