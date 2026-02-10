ba3809c796411ff5766d9e34930a14ce45a22e04
Frank xavio <frank2006x@gmail.com>
2026-02-10 19:37:57 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (70.62%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n==1) return 1;
-        int dp[n];
-        dp[0]=1;
-        dp[1]=2;
+        if(n<=2) return n;
+        int a=1;
+        int b=2;
         for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=a+b;
+            a=b;
+            b=temp;
         }
-        return dp[n-1];
+        return b;
     }
 };
