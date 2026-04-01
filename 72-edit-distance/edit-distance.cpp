c6e117e5667918dd0d4d4a09997dca6bf5a25e6b
Frank xavio <frank2006x@gmail.com>
2026-04-01 10:34:45 +0530

    Time: 13 ms (20.14%) | Memory: 13.3 MB (29.55%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
-    int solve(string w1,string w2,int i,int j,vector<vector<int>> &dp){
-        if(j<0){
-            return i+1;
-        }
-        if(i<0){
-            return j+1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(w1[i]==w2[j]){
-            return dp[i][j]=solve(w1,w2,i-1,j-1,dp);
-        }
-        return dp[i][j]=1+min({solve(w1,w2,i,j-1,dp),solve(w1,w2,i-1,j,dp),solve(w1,w2,i-1,j-1,dp)});
-    }
+    
     int minDistance(string w1, string w2) {
         int n=w1.size();
         int m=w2.size();
@@ -34,12 +22,7 @@ public:
                 }
             }
         }
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                cout<<dp[i][j]<<" ";
-            }
-            cout<<endl;
-        }
+        
         
         return dp[n][m];
     }
