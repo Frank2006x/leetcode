50b8f5c74bc7880daad6de9a94e9088a7e5f0d69
Frank xavio <frank2006x@gmail.com>
2026-04-02 07:57:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (5.14%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.go
@@ -6,21 +6,7 @@ func max(a, b int) int {
 	return b
 }
 
-func solve(prices []int, index int, buy int, dp [][]int) int {
-	if index == len(prices) {
-		return 0
-	}
-	if dp[index][buy] != -1 {
-		return dp[index][buy]
-	}
-	if buy == 1 {
-		dp[index][buy] = max(-prices[index]+solve(prices, index+1, 0, dp), solve(prices, index+1, 1, dp))
-	} else {
 
-		dp[index][buy] = max(prices[index]+solve(prices, index+1, 1, dp), solve(prices, index+1, 0, dp))
-	}
-	return dp[index][buy]
-}
 
 func maxProfit(prices []int) int {
 	n := len(prices)
